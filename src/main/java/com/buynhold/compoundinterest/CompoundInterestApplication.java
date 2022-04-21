0698bec17731728690d60495977d8a2ac6259860
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-04-21 14:59:24 -0300

    first commit

---
+++ src/main/java/com/buynhold/compoundinterest/CompoundInterestApplication.java
@@ -0,0 +1,13 @@
+package com.buynhold.compoundinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoundInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompoundInterestApplication.class, args);
+	}
+
+}
